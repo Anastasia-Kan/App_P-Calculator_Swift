6c9d75a8748a39f0bb72cfa536bef3fce1e91a92
anastasia.kantor <anastasia.kantor@gmail.com>
2020-11-09 21:27:23 +0100

    saving last selected tab

--- App_Pressure-Calculator/ViewControllers/MainTabBarVC.swift
+++ App_Pressure-Calculator/ViewControllers/MainTabBarVC.swift
@@ -5,21 +5,26 @@
 //  Created by Anastasia Kantor on 2020-11-09.
 //
 
+import Foundation
 import UIKit
 
-class MainTabBarVC: UITabBarController, UITabBarDelegate {
-
-    let ruby = RubyVC()
-    let diamond = DiamondVC()
-
-    let tabs = UITabBarController()
+class MainTabBarVC: UITabBarController, UITabBarControllerDelegate {
     
+    let selectedTabIndexKey = "selectedTabIndex"
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //MainTabBarVC.UITabBarDelegate = ?=?????
+        self.delegate = self
 
-        // Do any additional setup after loading the view.
+        // Load the last selected tab if the key exists in the UserDefaults
+        if UserDefaults.standard.object(forKey: self.selectedTabIndexKey) != nil {
+            self.selectedIndex = UserDefaults.standard.integer(forKey: self.selectedTabIndexKey)
+        }
     }
-    
+
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+        // Save the selected index to the UserDefaults
+        UserDefaults.standard.set(self.selectedIndex, forKey: self.selectedTabIndexKey)
+        UserDefaults.standard.synchronize()
+    }
 }
