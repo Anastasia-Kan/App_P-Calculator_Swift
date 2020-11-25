16eccb2fbfebe5cccf7a609e64e16064538d86f2
anastasia.kantor <anastasia.kantor@gmail.com>
2020-11-25 13:29:03 +0100

    Button "Done" add to Keyboard - first attempt

--- App_Pressure-Calculator/ViewControllers/RubyVC.swift
+++ App_Pressure-Calculator/ViewControllers/RubyVC.swift
@@ -7,9 +7,6 @@
 
 import UIKit
 
-class CellClass: UITableViewCell {
-}
-
 class RubyVC: UIViewController {
     // MARK: — Outlets
     @IBOutlet weak var refRuby: UITextField!
@@ -30,82 +27,93 @@ class RubyVC: UIViewController {
     
     var Pressure = 0.0
    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        refRuby.delegate = self
-        refTemp.delegate = self
-        gotRuby.delegate = self
-        gotTemp.delegate = self
-        
-        calcP.layer.cornerRadius = 10
-        calcP.clipsToBounds = true
-        
-        /*gotRuby.addTarget(self, action: #selector(RubyVC.textFieldDidChange(_:)), for: .editingChanged)*/  //Continue here!!!
-        if let selectedCalibration = UserDefaults.standard.value(forKey: "selectedCalibration"){
-                     let calibration = selectedCalibration as! Int
-                     calibrationSegments.selectedSegmentIndex = calibration
-                 }
-         
-         if let refTempSelectedScale = UserDefaults.standard.value(forKey: "refTempSelectedScale"){
-             let refTScale = refTempSelectedScale as! Int
-                     refTempScale.selectedSegmentIndex = refTScale
-                 }
-         
-         if let gotTempSelectedScale = UserDefaults.standard.value(forKey: "gotTempSelectedScale"){
-             let gotTScale = gotTempSelectedScale as! Int
-                     gotTempScale.selectedSegmentIndex = gotTScale
-                 }
-         selectingCalibration(calibrationSegments)
-         
+    override func viewDidLoad()
+    {
+            super.viewDidLoad()
+            refRuby.delegate = self
+            refTemp.delegate = self
+            gotRuby.delegate = self
+            gotTemp.delegate = self
+            
+            calcP.layer.cornerRadius = 10
+            calcP.clipsToBounds = true
 
+            if let selectedCalibration = UserDefaults.standard.value(forKey: "selectedCalibration")
+            {
+                let calibration = selectedCalibration as! Int
+                calibrationSegments.selectedSegmentIndex = calibration
+            }
+             
+            if let refTempSelectedScale = UserDefaults.standard.value(forKey: "refTempSelectedScale")
+            {
+                let refTScale = refTempSelectedScale as! Int
+                refTempScale.selectedSegmentIndex = refTScale
+            }
+             
+            if let gotTempSelectedScale = UserDefaults.standard.value(forKey: "gotTempSelectedScale")
+            {
+                let gotTScale = gotTempSelectedScale as! Int
+                gotTempScale.selectedSegmentIndex = gotTScale
+            }
+             selectingCalibration(calibrationSegments)
     }
     
     // CHANGING text in TextField
     @IBAction func gotTempChanged(_ sender: Any) {
+            calculateP((Any).self)
     }
-    
+        
+    @IBAction func gotRubyChanged(_ sender: Any) {
+            calculateP((Any).self)
+    }
+        
+    @IBAction func refTempChanged(_ sender: Any) {
+            calculateP((Any).self)
+    }
     
 
     // Input info: Excluding all caracters except for decimal NUMBERS
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        
-        let invalidCaracters = CharacterSet(charactersIn: "0123456789.-,").inverted
-        
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool
+    {
+        let invalidCaracters = CharacterSet(charactersIn: "0123456789.,-").inverted
         return (string.rangeOfCharacter(from: invalidCaracters) == nil)
     }
     
-    /*func textChanged(_ sender: UITextField) {
-        textField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
-       }*/
-   
-    // To dismiss Keybord
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+   // To dismiss Keybord
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
         view.endEditing(true)
-       
+           
         if (refRuby.text == "") {
-            refRuby.text = "694.22"
+                refRuby.text = "694.22"
         }
         if (gotRuby.text == "") {
-            gotRuby.text = "694.22"
+                gotRuby.text = "694.22"
         }
-        if (refTemp.text == "") {
-           if (refTScale == "K") {
-            refTemp.text = "298"
+        if (refTemp.text == "")
+        {
+            if (refTScale == "K")
+            {
+                refTemp.text = "298"
             }
-            if (refTScale == "C") {
-             refTemp.text = "25"
-             }
+            if (refTScale == "C")
+            {
+                refTemp.text = "25"
+            }
         }
-        if (gotTemp.text == "") {
-            if (gotTScale == "K") {
-             gotTemp.text = "298"
-             }
-             if (gotTScale == "C") {
-              gotTemp.text = "25"
-              }
+        if (gotTemp.text == "")
+        {
+            if (gotTScale == "K")
+            {
+                gotTemp.text = "298"
+            }
+            if (gotTScale == "C")
+            {
+                gotTemp.text = "25"
+            }
         }
     }
-        
+            
         
         
     // MARK: — Equations
@@ -130,94 +138,97 @@ class RubyVC: UIViewController {
     
     // MARK: - IBActions
     
-    @IBAction func selectingCalibration(_ sender: UISegmentedControl) {
+    @IBAction func selectingCalibration(_ sender: UISegmentedControl)
+    {
         UserDefaults.standard.set(sender.selectedSegmentIndex, forKey: "selectedCalibration")
 
         if(calibrationSegments.selectedSegmentIndex == 0)
         {
-          let segmentControl = UserDefaults.standard
-          selectedCalibration = "Mao-hydro"
-         }
-         else if(calibrationSegments.selectedSegmentIndex == 1)
-         {  let segmentControl = UserDefaults.standard
+            let segmentControl = UserDefaults.standard
+            selectedCalibration = "Mao-hydro"
+        }
+        else if(calibrationSegments.selectedSegmentIndex == 1)
+        {   let segmentControl = UserDefaults.standard
             selectedCalibration = "Mao-non-hydro"
-         }
-         else if(calibrationSegments.selectedSegmentIndex == 2)
-         {  let segmentControl = UserDefaults.standard
+        }
+        else if(calibrationSegments.selectedSegmentIndex == 2)
+        {   let segmentControl = UserDefaults.standard
             selectedCalibration = "Shen"
-         }
+        }
     }
     
     
-    @IBAction func selectingRefTempScale(_ sender: UISegmentedControl) {
+    @IBAction func selectingRefTempScale(_ sender: UISegmentedControl)
+    {
         UserDefaults.standard.set(sender.selectedSegmentIndex, forKey: "refTempSelectedScale")
-      
+          
         if(refTempScale.selectedSegmentIndex == 0)
-        { let refT = UserDefaults.standard
-          refTScale = "C"
-         }
-         else if(refTempScale.selectedSegmentIndex == 1)
-         { let refT = UserDefaults.standard
-           refTScale = "K"
-         }
+        {   let refT = UserDefaults.standard
+            refTScale = "C"
+        }
+        else if(refTempScale.selectedSegmentIndex == 1)
+        {   let refT = UserDefaults.standard
+            refTScale = "K"
+        }
     }
     
     
-    @IBAction func selectingGotTempScale(_ sender: UISegmentedControl) {
+    @IBAction func selectingGotTempScale(_ sender: UISegmentedControl)
+    {
         UserDefaults.standard.set(sender.selectedSegmentIndex, forKey: "gotTempSelectedScale")
 
         if(gotTempScale.selectedSegmentIndex == 0)
-        { let gotT = UserDefaults.standard
-          gotTScale = "C"
-         }
-         else if(refTempScale.selectedSegmentIndex == 1)
-         {  let gotT = UserDefaults.standard
+        {   let gotT = UserDefaults.standard
+            gotTScale = "C"
+        }
+        else if(refTempScale.selectedSegmentIndex == 1)
+        {   let gotT = UserDefaults.standard
             gotTScale = "K"
-         }
+        }
     }
     
-    @IBAction func calculateP(_ sender: Any) {
-        
-        view.endEditing(true)
-        
-        
-        var lambda0 = Double(refRuby.text!.doubleValue)
+    @IBAction func calculateP(_ sender: Any)
+    {
 
+        let lambda0 = Double(refRuby.text!.doubleValue)
         var RT = Double(refTemp.text!.doubleValue)
-
-        var lambda = Double(gotRuby.text!.doubleValue)
-
+        let lambda = Double(gotRuby.text!.doubleValue)
         var T = Double(gotTemp.text!.doubleValue)
 
-        if (gotTScale == "C") {
-             T += 273
-            }
-        if (refTScale == "C") {
-             RT += 273
-            }
-        
-        // Checking that all the numbers are in allowed ranges
-        if (690...800).contains(lambda0) {
+        if (gotTScale == "C")
+        {
+            T += 273
+        }
+        if (refTScale == "C")
+        {
+            RT += 273
+        }
+            
+    // Checking that all the numbers are in allowed ranges
+        if (690...800).contains(lambda0)
+        {
             print("everything is ok")
-        } else {print("something is wrong")
+        } else {
             resultP.text = "Check your values"
             return
         }
-        if (10...310).contains(RT) {
+        if (10...310).contains(RT)
+        {
             print("everything is ok")
-        } else {print("something is wrong")
+        } else {
             resultP.text = "Check your values"
             return
         }
-        if (690...1500).contains(lambda) {
+        if (690...1500).contains(lambda)
+        {
             print("everything is ok")
-        } else {print("something is wrong")
+        } else {
             resultP.text = "Check your values"
             return
         }
-        
-        // Making T-corrections for lambda and lambda0
-        
+            
+            // Making T-corrections for lambda and lambda0
+            
         let deltaT = T - 296
         let deltaTsqr = deltaT * deltaT
         let deltaTcub = deltaT * deltaT * deltaT
@@ -226,65 +237,111 @@ class RubyVC: UIViewController {
         let deltaRTcub = deltaRT * deltaRT * deltaRT
         var corrLambda = -0.887
         var corrLambda0 = -0.887
-        
-        
-        if (50...296).contains(RT){
+            
+            
+        if (50...296).contains(RT)
+        {
             corrLambda0 = (0.00664 * deltaRT) + (6.76e-6 * deltaRTsqr) - (2.33e-8 * deltaRTcub)
         }
-        if (RT >= 296) {
+        if (RT >= 296)
+        {
             corrLambda0 = (0.00746 * deltaRT) - (3.01e-6 * deltaRTsqr) + (8.76e-9 * deltaRTcub)
         }
         
         let lam0 = lambda0 - corrLambda0
         
-        if (50...296).contains(T){
+        if (50...296).contains(T)
+        {
             corrLambda = (0.00664 * deltaT) + (6.76e-6 * deltaTsqr) - (2.33e-8 * deltaTcub)
         }
-        if (T >= 296) {
+        if (T >= 296)
+        {
             corrLambda = (0.00746 * deltaT) - (3.01e-6 * deltaTsqr) + (8.76e-9 * deltaTcub)
         }
         
         let lam = lambda - corrLambda
-        
-        // Calculating pressure according to chosen equation with T-corrections
-        
-        if selectedCalibration == "Mao-hydro" {
+            
+            // Calculating pressure according to chosen equation with T-corrections
+            
+        if selectedCalibration == "Mao-hydro"
+        {
             Mao(A: 1904, B: 7.665, lambda: lam, lambda0: lam0)
         }
-        if selectedCalibration == "Mao-non-hydro" {
+        if selectedCalibration == "Mao-non-hydro"
+        {
             Mao(A: 1904, B: 5, lambda: lam, lambda0: lam0)
         }
-        if selectedCalibration == "Shen" {
+        if selectedCalibration == "Shen"
+        {
             Shen(A: 1870, B: 5.63, lambda: lam, lambda0: lam0)
         }
- 
+     
         let P = ((Pressure * 100).rounded()) / 100
         resultP.text = String(P)
     }
 }
 
 // Add TextField Delegates
-extension RubyVC : UITextFieldDelegate {
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+extension RubyVC : UITextFieldDelegate
+{
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool
+    {
         textField.resignFirstResponder()
         return true
     }
 }
 
-extension String {
+extension String
+{
     static let numberFormatter = NumberFormatter()
-    var doubleValue: Double {
+    var doubleValue: Double
+    {
         String.numberFormatter.decimalSeparator = "."
-        if let result =  String.numberFormatter.number(from: self) {
+        if let result =  String.numberFormatter.number(from: self)
+        {
             return result.doubleValue
         } else {
             String.numberFormatter.decimalSeparator = ","
-            if let result = String.numberFormatter.number(from: self) {
+            if let result = String.numberFormatter.number(from: self)
+            {
                 return result.doubleValue
             }
         }
         return 0
+    }
+}
+
+extension UITextField{
+    
+    @IBInspectable var doneAccessory: Bool{
+        get{
+            return self.doneAccessory
+        }
+        set (hasDone) {
+            if hasDone{
+                addDoneButtonOnKeyboard()
+            }
+        }
+    }
+    
+    func addDoneButtonOnKeyboard()
+    {
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        doneToolbar.barStyle = .default
+        
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.doneButtonAction))
+        
+        let items = [flexSpace, done]
+        doneToolbar.items = items
+        doneToolbar.sizeToFit()
+        
+        self.inputAccessoryView = doneToolbar
+    }
+    
+    @objc func doneButtonAction()
+    {
+        self.resignFirstResponder()
     }
 }
 
